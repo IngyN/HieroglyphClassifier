16ed2189102e9595eaa15216a2c583f8ce8509cb
AliaMYH <aliahassan95@aucegypt.edu>
2017-05-11 18:31:35 +0200

    Initial , dataset and code

---
+++ heiro_aug.py
@@ -0,0 +1,143 @@
+import keras
+import numpy as np
+import time
+
+from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau     # Reference: https://keras.io/callbacks/
+from keras.applications import *
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
+from keras.constraints import maxnorm
+import tensorflow as tf
+import keras.backend.tensorflow_backend as ktf
+from keras.optimizers import Adam, SGD
+
+current = time.strftime("%c")
+
+def load_dataset():
+    validation_data_dir = './Heiroglyphs/'
+
+    classes = []
+    for subdir in sorted(os.listdir(validation_data_dir)):
+        if os.path.isdir(os.path.join(validation_data_dir, subdir)):
+            classes.append(subdir)
+
+    class_indices = dict(zip(classes, range(len(classes))))
+
+    X_val = []
+
+    # Extracting validation dat
+    i = 0
+    y_val = []
+    for subdir in classes:
+        subpath = os.path.join(validation_data_dir, subdir)
+        for fname in sorted(os.listdir(subpath)):
+            y_val.append(class_indices[subdir])
+
+            # Load image as numpy array and append it to X_val
+            img = load_img(os.path.join(subpath, fname), target_size=(img_width, img_height))
+            x = img_to_array(img)
+            X_val.append(x)
+
+            i += 1
+
+    Y_val = np_utils.to_categorical(y_val)
+    X_val = np.asarray(X_val, dtype='float32')
+    return classes, X_val, Y_val
+
+#LOADING DATASET 
+classes, X_train, Y_train = load_dataset()
+
+#Labels to categorical 
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+
+
+batch_size = 32
+
+tr_datagen = image.ImageDataGenerator(                
+    rotation_range = 5,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    vertical_flip = False)
+
+tr_datagen.fit(X_train)
+
+tr_generator = tr_datagen.flow_from_directory(
+        './Heiroglyphs/',  # this is the target directory
+        target_size=(32,32),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+val_datagen = image.ImageDataGenerator(featurewise_center = False,featurewise_std_normalization = False,rescale = None)
+
+# this is a similar generator, for validation data
+val_generator = val_datagen.flow_from_directory(
+        './validation/',
+        target_size=(32,32),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+############## Creating Model ################
+
+# initialize the model
+model = Sequential()
+
+# first set of CONV => RELU => POOL
+model.add(Convolution2D(20, 5, 5, border_mode="same",
+        input_shape=(depth, height, width)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+# second set of CONV => RELU => POOL
+model.add(Convolution2D(50, 5, 5, border_mode="same"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+# set of FC => RELU layers
+model.add(Flatten())
+model.add(Dense(500))
+model.add(Activation("relu"))
+
+# softmax classifier
+model.add(Dense(classes))
+model.add(Activation("softmax"))
+
+model.load_weights("lenet_weights.hdf5")
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+# for layer in base_model.layers:
+#      layer.trainable = False
+
+sgd = SGD(lr=0.01, momentum=0.6, decay=0.000001, nesterov=True)
+
+model.compile(loss = 'categorical_crossentropy', optimizer = sgd,  metrics = ['accuracy'])
+model.summary()
+
+
+# Training Model
+
+checkpointer = ModelCheckpoint(filepath = "heiro.hdf5", verbose = 1, save_best_only = True, monitor = 'val_loss')   
+tb = TensorBoard(log_dir='./heiro_logs/'+ current, histogram_freq=0, write_graph=True, write_images=False)
+
+#reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.1, patience = 5, min_lr = 1e-6)
+
+history = model.fit_generator(
+        tr_generator,
+        steps_per_epoch=1500,
+        epochs=epoch_count,
+        validation_data=val_generator,
+        validation_steps=150,
+        callbacks = [tb, checkpointer])
+
+
+
+
+score = model.evaluate_generator(val_generator)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+
