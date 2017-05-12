281c096b2cd858e75d61509eced83c11f4500a51
Ingy <ingyesa@aucegypt.edu>
2017-05-12 19:45:06 +0300

    tuning

--- testingkanji.py
+++ testingkanji.py
@@ -38,54 +38,47 @@ tr_generator = tr_datagen.flow_from_directory(
     color_mode='grayscale',
     class_mode='categorical')
 
-# validation data generation and preprocessing
-val_datagen = image.ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True, rescale=None,
-                                       rotation_range=15,
-                                       width_shift_range=0.12,
-                                       height_shift_range=0.12,
-                                       shear_range=0.2,
-                                       zoom_range=0.2,
-                                       fill_mode='nearest',
-                                       horizontal_flip=True)
-# this is a similar generator, for validation data
-val_generator = val_datagen.flow_from_directory(
-    './Heiro_val/',
-    target_size=image_shape,
-    batch_size=batch_size,
-    class_mode='categorical',
-    color_mode='grayscale')
-
 # Load architecture
 f = open('augmented_model_architecture.json', 'r')
 model = model_from_json(f.read())
 f.close()
-
-# Load weights
-model.load_weights('theweights.hdf5', by_name=True)
-model.summary()
-
 # Stochastic Gradient Descent optimizer.
 sgd = SGD(lr=0.00002, momentum=0.7, decay=0.0001, nesterov=True)
-
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-# reading current time to differentiate files in logs and submissions.
 current = time.strftime("%c")
 
-checkpointer = ModelCheckpoint(filepath="theweights.hdf5", verbose=1, save_best_only=True, monitor='val_loss')
-tb = TensorBoard(log_dir='./logsKanji/' + current, histogram_freq=0, write_graph=True, write_images=False)
 
-# Using reduce Learning rate on plateau to try to prevent saturation and overfitting by reducing the learning rate.
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4, min_lr=1e-6)
+# Using the testing file to create a submission
 
-# Train model
-model.fit_generator(
-    tr_generator,
-    samples_per_epoch=700,  # amount of data we want to train on
-    nb_epoch=epoch_count,
-    validation_data=val_generator,
-    nb_val_samples=200,  # amount of data we want to validate on
-    callbacks=[tb, checkpointer])
+# Load most recent weights generated
+model.load_weights('./theweights.hdf5')
+model.compile(loss = 'categorical_crossentropy', optimizer = sgd,  metrics = ['accuracy'])
+
+# copy to my folder to keep track of all weights
+cmd = 'cp theweights.hdf5 theweights/my_weights_'+ current[0:5] +'.hdf5'
+os.system(cmd)
+
+l=[None]*40
+
+for key,value in tr_generator.class_indices.items():
+    l[value]=key
+
+f = open('mysub'+current[0:5], 'w')
+
+f.write('Id,Prediction\n')
+
+for i in range(0, 12):
+    imgn = 'test_%d' % (i,)
+    imgn += '.jpg'
+    img_p = './test/' + imgn
+    img = image.load_img(img_p, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    # Predict for test image
+    preds = model.predict(x)
+
+    # write the result line using highest probability from the classifier
+    f.write(imgn + ',' + l[np.argmax(preds)] + '\n')
 
 
-
+f.close()
